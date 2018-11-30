da69ec78760628c5ffbd29f123268a7fce522fe8
RyanVerhey <ryan90v@gmail.com>
2018-11-29 22:25:11 -0500

    Adding method for updating tile value

--- app/sudoku/board/tile.rb
+++ app/sudoku/board/tile.rb
@@ -3,4 +3,10 @@ class Sudoku::Board::Tile
   def initialize(value)
     @value = value
   end
+
+  def value=(new_value)
+    # TODO: Make a custom exception
+    raise(ArgumentError.new("Cannot reassign an assigned tile")) if !@value.nil?
+    @value = new_value
+  end
 end
