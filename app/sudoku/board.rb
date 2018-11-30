7dc8ad3d1e6e52efaee9c8b001c738510c372256
RyanVerhey <ryan90v@gmail.com>
2018-11-29 22:36:49 -0500

    Adding the coordinates for the sub-squares

--- app/sudoku/board.rb
+++ app/sudoku/board.rb
@@ -2,6 +2,18 @@
 class Sudoku::Board
   attr_reader :board
 
+  SQUARE_COORDS = [
+    [(0..2), (0..2)],
+    [(0..2), (3..5)],
+    [(0..2), (6..8)],
+    [(3..5), (0..2)],
+    [(3..5), (3..5)],
+    [(3..5), (6..8)],
+    [(6..8), (0..2)],
+    [(6..8), (3..5)],
+    [(6..8), (6..8)]
+  ]
+
   def initialize(puzzle)
     tiles = puzzle.map { |value| Tile.new(value) }
     @board = Matrix[tiles.each_slice(9).to_a]
