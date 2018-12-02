5f9be963a7fe02e5e01f9e76ffc6bb632160ff4a
RyanVerhey <ryan90v@gmail.com>
2018-12-02 13:16:27 -0500

    Added new puzzle and solution

--- run.rb
+++ run.rb
@@ -4,15 +4,26 @@ Dir["./app/*.rb"].each { |file| require file }
 Dir["./app/sudoku/*.rb"].each { |file| require file }
 Dir["./app/sudoku/board/*.rb"].each { |file| require file }
 
-easy_sudoku_puzzle = [nil, nil, nil, nil, nil, nil, 1, 7, nil,
-                      6, nil, 4, 7, 5, nil, 8, nil, 9,
-                      nil, 5, 9, nil, 3, nil, nil, nil, nil,
-                      nil, nil, nil, nil, nil, nil, 6, nil, 7,
-                      3, nil, 5, 1, 2, 7, 9, nil, 4,
-                      4, nil, 1, nil, nil, nil, nil, nil, nil,
-                      nil, nil, nil, nil, 6, nil, 2, 4, nil,
-                      1, nil, 6, nil, 7, 9, 3, nil, 8,
-                      nil, 3, 8, nil, nil, nil, nil, nil, nil]
+# PUzzle Link: https://www.websudoku.com/?level=1&set_id=2959290249
+easy_sudoku_puzzle = [nil, nil, 6, 9, 8, 2, 4, 5, nil,
+                      nil, nil, 5, 4, nil, nil, nil, nil, 3,
+                      nil, nil, 7, nil, nil, nil, 1, 9, 2,
+                      nil, nil, 1, 2, 9, nil, nil, nil, nil,
+                      nil, 4, nil, 6, nil, 5, nil, 2, nil,
+                      nil, nil, nil, nil, 1, 8, 9, nil, nil,
+                      9, 1, 8, nil, nil, nil, 5, nil, nil,
+                      5, nil, nil, nil, nil, 7, 8, nil, nil,
+                      nil, 7, 4, 8, 5, 9, 2, nil, nil]
+
+# expected = [1, 3, 6, 9, 8, 2, 4, 5, 7,
+#             2, 9, 5, 4, 7, 1, 6, 8, 3,
+#             4, 8, 7, 5, 6, 3, 1, 9, 2,
+#             7, 5, 1, 2, 9, 4, 3, 6, 8,
+#             8, 4, 9, 6, 3, 5, 7, 2, 1,
+#             6, 2, 3, 7, 1, 8, 9, 4, 5,
+#             9, 1, 8, 3, 2, 6, 5, 7, 4,
+#             5, 6, 2, 1, 4, 7, 8, 3, 9,
+#             3, 7, 4, 8, 5, 9, 2, 1, 6]
 
 puzzle = Sudoku.new(easy_sudoku_puzzle)
 result_board = puzzle.solve
