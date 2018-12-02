ed494466ca88267184960fb0fafb43702233b6fa
RyanVerhey <ryan90v@gmail.com>
2018-12-02 15:01:24 -0500

    Adding nils to test board to make tests more robust

--- test/sudoku/board_test.rb
+++ test/sudoku/board_test.rb
@@ -1,14 +1,14 @@
 class Sudoku::BoardTest < MiniTest::Test
   def setup
-    board = [1, 3, 6, 9, 8, 2, 4, 5, 7,
-             2, 9, 5, 4, 7, 1, 6, 8, 3,
-             4, 8, 7, 5, 6, 3, 1, 9, 2,
-             7, 5, 1, 2, 9, 4, 3, 6, 8,
-             8, 4, 9, 6, 3, 5, 7, 2, 1,
-             6, 2, 3, 7, 1, 8, 9, 4, 5,
-             9, 1, 8, 3, 2, 6, 5, 7, 4,
-             5, 6, 2, 1, 4, 7, 8, 3, 9,
-             3, 7, 4, 8, 5, 9, 2, 1, 6]
+    board = [nil, nil, 6, 9, 8, 2, 4, 5, nil,
+             nil, nil, 5, 4, nil, nil, nil, nil, 3,
+             nil, nil, 7, nil, nil, nil, 1, 9, 2,
+             nil, nil, 1, 2, 9, nil, nil, nil, nil,
+             nil, 4, nil, 6, nil, 5, nil, 2, nil,
+             nil, nil, nil, nil, 1, 8, 9, nil, nil,
+             9, 1, 8, nil, nil, nil, 5, nil, nil,
+             5, nil, nil, nil, nil, 7, 8, nil, nil,
+             nil, 7, 4, 8, 5, 9, 2, nil, nil]
 
     @board = Sudoku::Board.new(board)
     @tile_index = [4, 1]
