a6e4ad21dbab9edb478c1bcd6b4eeec46fe01183
RyanVerhey <ryan90v@gmail.com>
2018-12-02 16:00:28 -0500

    Added some methods to tile

--- app/sudoku/board/tile.rb
+++ app/sudoku/board/tile.rb
@@ -1,15 +1,23 @@
 # Handles behavior/state for tiles
 class Sudoku::Board::Tile
   attr_reader :value
+  attr_accessor :possibilities
 
   def initialize(value, board = nil)
     @value = value
     @board = board
+    @possibilities = []
   end
 
   def value=(new_value)
     # TODO: Make a custom exception
     raise(ArgumentError.new("Cannot reassign an assigned tile")) if !@value.nil?
     @value = new_value
+    @possibilities = []
+    @value
+  end
+
+  def empty?
+    @value.nil?
   end
 end
--- test/sudoku/board/tile_test.rb
+++ test/sudoku/board/tile_test.rb
@@ -1,2 +1,35 @@
 class Sudoku::Board::TileTest < MiniTest::Test
+  def setup
+    @tile = Sudoku::Board::Tile.new(5)
+    @empty_tile = Sudoku::Board::Tile.new(nil)
+  end
+
+  class ValueEquals < Sudoku::Board::TileTest
+    def test_raises_an_error_if_the_tile_has_a_value
+      assert_raises ArgumentError do
+        @tile.value = 6
+      end
+
+      # Nothing happens
+      assert @empty_tile.value = 6
+    end
+
+    def test_sets_the_new_value
+      @empty_tile.value = 6
+      assert_equal 6, @empty_tile.value
+    end
+
+    def test_resets_the_tiles_possibilities
+      @empty_tile.possibilities += (1..5).to_a
+      @empty_tile.value = 6
+      assert @empty_tile.possibilities.empty?
+    end
+  end
+
+  class Empty < Sudoku::Board::TileTest
+    def test_returns_whether_or_not_the_tile_is_empty
+      refute @tile.empty?
+      assert @empty_tile.empty?
+    end
+  end
 end
